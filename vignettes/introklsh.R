5e18d88e3e355b06667e471c04faeb489fdae624
Rebecca Steorts <beka@stat.duke.edu>
2018-04-23 12:42:04 -0400

    updated vignette

--- vignettes/introklsh.R
+++
@@ -1,9 +0,0 @@
-## ---- echo=TRUE, message=FALSE, knitr::opts_chunk$set(cache=TRUE)--------
-library(RecordLinkage)
-library(klsh)
-library(plyr)
-data(RLdata500)
-head(RLdata500)
-data.500 <- RLdata500[-c(2,4)]
-head(data.500)
-
